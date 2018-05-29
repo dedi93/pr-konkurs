85050e8a5e9e7db8d9227b9ced38553c3f4d327d
dedi93 <chmiele@gmail.com>
2018-05-29 06:17:26 -0700

    Updated TAGS

--- global.h
+++ global.h
@@ -1,7 +1,13 @@
-#define TAG_EMPTY -1
+#define EMPTY_TAG -1
+#define FINNISH_TAG 0
+#define DOC_ACK_TAG 1
+#define REQ_DOC_TAG 2
+#define SALON_ACK_TAG 3
+#define REQ_SALON_TAG 4
+
 
 typedef struct Message {
-  int tag;
+  //int tag;
   int timestamp;
   int data;   //doctor's id or how many sit's in salon
 } Message;
